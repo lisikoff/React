3a00826c2bb584e7b849aed703f7e16a5fd0142b
lisikoff <max7im13@tut.by>
2018-09-17 00:49:51 +0300

    test

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -52,6 +53,7 @@ class Main extends Component {
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders}/>} />} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
